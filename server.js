599946e0115d50fca17d5cc69db2eafad3ab66df
Mattgh91 <mattgh9152@gmail.com>
2019-07-11 23:09:26 +0100

    Add User Model & validation - register/user route

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Call Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact Keep API' }));
 
 // Define Routes...
