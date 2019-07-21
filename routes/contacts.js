4dd4dde0b54223835c7ed957cd1096d59f488fa4
Mattgh91 <mattgh9152@gmail.com>
2019-07-21 21:04:13 +0100

    Lint + fix bug in contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -61,39 +61,37 @@ router.post('/',
 // @route       PUT api/contacts/:id
 // @desc        Update contact
 // @access      Private
-router.put('/:id', auth, [], async (req, res) => {
-    res.send('Update contact!');
-
+router.put('/:id', auth, async (req, res) => {
     const { name, email, phone, type } = req.body;
 
     // Build contact object
     const contactFields = {};
-    if(name) contactFields.name = name;
-    if(email) contactFields.email = email;
-    if(phone) contactFields.phone = phone;
-    if(type) contactFields.type = type;
+    if (name) contactFields.name = name;
+    if (email) contactFields.email = email;
+    if (phone) contactFields.phone = phone;
+    if (type) contactFields.type = type;
 
     try {
         let contact = await Contact.findById(req.params.id);
 
-        if(!contact) return res.status(404).json({ msg: 'Contact not found!' });
+        if (!contact) return res.status(404).json({ msg: 'Contact not found' });
 
-        // Make sure contact belongs to user
-        if(contact.user.toString() !== res.user.id) {
-            return res.status(401).json({ msg: 'Not authorised!' });
+        // Make sure user owns contact
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not authorized' });
         }
 
-        // Update contact
-        contact = await Contact.findByIdAndUpdate(req.params.id,
+        contact = await Contact.findByIdAndUpdate(
+            req.params.id,
             { $set: contactFields },
-            { new: true });
+            { new: true }
+        );
 
         res.json(contact);
-    } catch(err) {
+    } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
     }
-
 });
 
 // @route       DELETE api/contacts/:id
