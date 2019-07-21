8a1ae9d844070e129debe9cce70a6538a9f5a4f3
Mattgh91 <mattgh9152@gmail.com>
2019-07-21 21:03:33 +0100

    Contact router

--- routes/contacts.js
+++ routes/contacts.js
@@ -52,7 +52,7 @@ router.post('/',
 
             res.json(contact);
         } catch (err) {
-            console.error(er.message);
+            console.error(err.message);
             res.status(500).send('Server Error');
         }
     }
@@ -61,8 +61,39 @@ router.post('/',
 // @route       PUT api/contacts/:id
 // @desc        Update contact
 // @access      Private
-router.put('/:id', (req, res) => {
+router.put('/:id', auth, [], async (req, res) => {
     res.send('Update contact!');
+
+    const { name, email, phone, type } = req.body;
+
+    // Build contact object
+    const contactFields = {};
+    if(name) contactFields.name = name;
+    if(email) contactFields.email = email;
+    if(phone) contactFields.phone = phone;
+    if(type) contactFields.type = type;
+
+    try {
+        let contact = await Contact.findById(req.params.id);
+
+        if(!contact) return res.status(404).json({ msg: 'Contact not found!' });
+
+        // Make sure contact belongs to user
+        if(contact.user.toString() !== res.user.id) {
+            return res.status(401).json({ msg: 'Not authorised!' });
+        }
+
+        // Update contact
+        contact = await Contact.findByIdAndUpdate(req.params.id,
+            { $set: contactFields },
+            { new: true });
+
+        res.json(contact);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
 });
 
 // @route       DELETE api/contacts/:id
