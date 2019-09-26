9ace2e77704c7e22892c671dd195b3ed24dd9d22
Mattgh91 <mattgh9152@gmail.com>
2019-09-26 17:35:00 +0100

    Register & Login forms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,11 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/register';
+import Login from './components/auth/login';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+
 
 const App = () => {
     return (
@@ -18,6 +21,8 @@ const App = () => {
                             <Switch>
                                 <Route exact path='/' component={Home} />
                                 <Route exact path='/about' component={About} />
+                                <Route exact path='/register' component={Register} />
+                                <Route exact path='/login' component={Login} />
                             </Switch>
                         </div>
                     </Fragment>
