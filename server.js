2f002850720d2dc31a5c6c72f9d670142e157bcb
Mattgh91 <mattgh9152@gmail.com>
2019-12-03 14:36:07 +0000

    server.js minor changes

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const path = require('path');
 
 const app = express();
 
-// Call Database
+// Connect Database
 connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// Define Routes...
-app.use('/api/users/', require('./routes/users'));
-app.use('/api/auth/', require('./routes/auth'));
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 // Serve static assets in production
@@ -20,9 +20,11 @@ if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
 }
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
