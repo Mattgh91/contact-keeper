641edaf30f4b46c3855c586551587e7fcde0b9dd
Mattgh91 <mattgh9152@gmail.com>
2019-12-03 13:23:42 +0000

    UPDATE contact. Final CRUD

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -80,6 +80,29 @@ const ContactState = props => {
 
     };
 
+    // Update contact
+    const updateContact = async contact => {
+        const config = {
+            headers: {
+                'Context-Type': 'application/json'
+            }
+        };
+
+        try {
+            const res = await axios.put(`/api/contacts/${contact._id}`, contact, config);
+
+            dispatch({
+                type: UPDATE_CONTACT,
+                payload: res.data,
+            });
+        } catch (err) {
+            dispatch({
+                type: CONTACT_ERROR,
+                payload: err.response.msg,
+            });
+        }
+    };
+
     // Clear Contacts
     const clearContacts = () => {
         dispatch({ type: CLEAR_CONTACTS });
@@ -93,11 +116,6 @@ const ContactState = props => {
     // Clear current contact
     const clearCurrent = () => {
         dispatch({ type: CLEAR_CURRENT });
-    };
-
-    // Update contact
-    const updateContact = contact => {
-        dispatch({ type: UPDATE_CONTACT, payload: contact });
     };
 
     // Filter Contacts
